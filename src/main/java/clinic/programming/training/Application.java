6bb80d60929cf75c7c8a592be44d81263183ebe3
thirumalai <makechange1997@gmail.com>
2020-05-26 15:42:14 +0530

    added StringUtils and Dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,23 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+	public int countWords(String statement){
+		String[] words = StringUtils.split(statement,' ');
+		return (words == null)?0:words.length;
+	}
+	
+	public void greet(){
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("hello");
+		
+		for(String say:list){
+			System.out.println(say);
+		}
+	}
+	
     
     public Application() {
         System.out.println ("Inside Application");
@@ -10,5 +27,8 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	app.greet();
+	int count = app.countWords("this is thirumalai. testing");
+	System.out.println("words count: "+count);
     }
 }
